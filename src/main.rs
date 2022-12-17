47b8c76151e2c18917245faac35f4cf7c2b587b6
Gustavo Noronha Silva <gustavo.noronha@collabora.com>
2022-12-16 21:17:14 -0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(decl_macro)]
+use structopt::StructOpt;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
@@ -6,10 +7,32 @@ mod file_probes;
 mod lupa;
 pub mod probe_serde;
 mod process;
+mod tracer;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "lupa", about = "Watch or record files opened by a process.")]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    Trace { pid: u64 },
+}
 
 fn main() {
+    let opt = Opt::from_args();
+
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::WARN)
         .finish();
+
     tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    if let Some(cmd) = opt.cmd {
+        match cmd {
+            Command::Trace { pid } => println!("pid: {}", pid),
+        }
+    }
 }
