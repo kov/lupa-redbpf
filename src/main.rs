1681412e0d418c8803a34206c070b699662ef358
Gustavo Noronha Silva <gustavo@noronha.dev.br>
2023-07-23 13:58:20 -0300

    Remove ui

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
 #![feature(decl_macro)]
 use anyhow::{bail, Result};
-use crossterm::{
-    event::{self, Event, KeyCode},
-    execute, style, terminal,
-};
-use std::{
-    collections::HashMap,
-    panic::{self, PanicInfo},
-    path::PathBuf,
-};
+use std::{collections::HashMap, path::PathBuf};
 use structopt::StructOpt;
 use tracer::Event as TraceEvent;
 use tracing::{trace, Level};
 use tracing_subscriber::FmtSubscriber;
-use tui::{backend, backend::CrosstermBackend, Terminal};
 
 use crate::tracer::Tracer;
 
@@ -33,10 +24,6 @@ enum Command {
 }
 
 fn main() -> Result<()> {
-    panic::set_hook(Box::new(|info| {
-        panic_hook(info);
-    }));
-
     let opt = Opt::from_args();
 
     let subscriber = FmtSubscriber::builder()
@@ -68,13 +55,12 @@ type OpenFilesMap = HashMap<(u64, u64), File>;
 type EventHistory = Vec<TraceEvent>;
 
 fn trace_pid(pid: u64) -> Result<()> {
-    let mut terminal = setup_terminal()?;
-
     let mut open_files = OpenFilesMap::new();
     let mut event_history = EventHistory::new();
 
     let tracer = Tracer::new(pid);
     for event in tracer {
+        println!("{:#?}", event);
         match &event {
             TraceEvent::FileOpen { pid, fd, path } => {
                 open_files.insert(
@@ -98,63 +84,5 @@ fn trace_pid(pid: u64) -> Result<()> {
         event_history.push(event);
     }
 
-    terminal.show_cursor()?;
-    restore_terminal()
-}
-
-fn setup_terminal() -> Result<Terminal<impl backend::Backend>> {
-    terminal::enable_raw_mode()?;
-
-    let mut stdout = std::io::stdout();
-
-    execute!(
-        stdout,
-        terminal::EnterAlternateScreen,
-        event::EnableMouseCapture
-    )?;
-
-    let terminal = Terminal::new(CrosstermBackend::new(stdout))?;
-
-    Ok(terminal)
-}
-
-fn restore_terminal() -> Result<()> {
-    terminal::disable_raw_mode()?;
-
-    execute!(
-        std::io::stdout(),
-        terminal::LeaveAlternateScreen,
-        event::DisableMouseCapture
-    )?;
-
     Ok(())
-}
-
-// Stolen froHEAVILY INSPIRED on https://github.com/Rigellute/spotify-tui/blob/93fd30fa55accc3df6f1c1548de28c5465b97074/src/main.rs#L95
-fn panic_hook(info: &PanicInfo<'_>) {
-    if cfg!(debug_assertions) {
-        let location = info.location().unwrap();
-
-        let msg = match info.payload().downcast_ref::<&'static str>() {
-            Some(s) => *s,
-            None => match info.payload().downcast_ref::<String>() {
-                Some(s) => &s[..],
-                None => "Box<Any>",
-            },
-        };
-
-        let stacktrace: String = format!("{:?}", backtrace::Backtrace::new()).replace('\n', "\n\r");
-
-        terminal::disable_raw_mode().unwrap();
-        execute!(
-            std::io::stdout(),
-            terminal::LeaveAlternateScreen,
-            style::Print(format!(
-                "thread '<unnamed>' panicked at '{}', {}\n\r{}",
-                msg, location, stacktrace
-            )),
-            event::DisableMouseCapture
-        )
-        .unwrap();
-    }
 }
